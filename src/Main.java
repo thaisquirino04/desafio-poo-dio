93e68b6bfe56c425b2d1635e17a6c002cd2cd6ee
thaisquirino04 <thaisquirino0402@gmail.com>
2022-07-23 13:12:31 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -20,9 +19,44 @@ public class Main {
         mentoria.setDescricao("descricao mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootecamp bootecamp = new Bootecamp();
+        bootecamp.setNome("Bootcamp Java Developer");
+        bootecamp.setDescricao("Descrição Bootecamp Java Developer");
+        bootecamp.getConteudos().add(curso1);
+        bootecamp.getConteudos().add(curso2);
+        bootecamp.getConteudos().add(mentoria);
+
+        Dev devThais = new Dev();
+        devThais.setNome("Thais");
+        devThais.increverBootecamp(bootecamp);
+        System.out.println("Conteudos Inscritos" + devThais.getConteudosInscritos());
+
+        devThais.progredir();
+        devThais.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos" + devThais.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Thais:" + devThais.getConteudosConcluidos());
+        System.out.println("XP:" + devThais.calcularTotalXp());
+
+        System.out.println("-------");
+
+        Dev devLaura = new Dev();
+        devLaura.setNome("Laura");
+        devLaura.increverBootecamp(bootecamp);
+        System.out.println("Conteudos Inscritos" + devLaura.getConteudosInscritos());
+
+        devLaura.progredir();
+        devLaura.progredir();
+        devLaura.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Concluidos Laura:" + devLaura.getConteudosConcluidos());
+        System.out.println("Conteudos Inscritos" + devLaura.getConteudosInscritos());
+        System.out.println("XP:" + devLaura.calcularTotalXp());
+
 
 
     }
